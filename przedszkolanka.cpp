f9f279756d4b3460e5d1ab6a8800f5731040bb5c
karol1ch <karol.chomoncik@gmail.com>
2016-08-24 22:22:57 +0200

    Poprawne rozwiazanie

--- przedszkolanka.cpp
+++ przedszkolanka.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-	cout<<"Hello world"<<endl;
+int main () 
+{	
+	int a, b, n;
+	cin>>n;
+	while(n--)
+	{
+		cin>>a>>b;
+		int a0=a, b0=b;
+		while(a!=b) 
+		{ 	
+			if (a<b) a+=a0;
+			else b+=b0;
+		}
+		cout<<a<<endl;
+	
+	}
+	
+	
+	
 	return 0;
 }
